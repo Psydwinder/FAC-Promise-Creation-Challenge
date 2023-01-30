55315ad7e013d16a7399c75af45662ba0b89bf25
Niete <nieteis@gmail.com>
2023-01-30 12:45:36 +0000

    Challenge completed

--- challenge/read-file.js
+++ challenge/read-file.js
@@ -1,7 +1,17 @@
 const fs = require("node:fs");
 
+//The code defines a function readFilePromise which takes a file path as an argument and returns a Promise that:
 function readFilePromise(filePath) {
-  //
+  return new Promise((resolve, reject) =>{
+    fs.readFile(filePath,(error,contents) =>{ //Returns the contents of a file using the fs.readFile method from the built-in fs module in Node.js.
+      if (error) { //If the fs.readFile operation is successful, the contents of the file are passed to the resolve function to indicate that the Promise is fulfilled.
+        reject (error);
+      } else { //If there is an error reading the file, the error is passed to the reject function to indicate that the Promise is rejected.
+        resolve(contents);
+      }
+    });
+  });
 }
 
+//The Promise returned by readFilePromise can be used to perform asynchronous file reading in a way that can be easily composed with other asynchronous operations.
 module.exports = { readFilePromise };
--- challenge/wait.js
+++ challenge/wait.js
@@ -1,5 +1,12 @@
 function wait(ms) {
-  //
-}
+  const promise = new Promise((resolve, reject) =>{
+    setTimeout(() =>{
+      resolve("done");}
+      ,ms);
+    });
+    return promise;
+  }
 
+ 
+  
 module.exports = { wait };
